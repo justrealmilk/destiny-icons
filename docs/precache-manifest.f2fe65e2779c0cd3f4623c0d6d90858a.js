29d8bf1cdb64b0930af907c1ccf9c0ebd9cf4c77
Tom Chapman <chapman@live.com.au>
2019-08-16 19:14:44 +1000

    .

---
+++ docs/precache-manifest.f2fe65e2779c0cd3f4623c0d6d90858a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b97db80faba43ff6baafb6ce2950f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8701392bad5e4cb3a303",
+    "url": "/static/css/main.befa693b.chunk.css"
+  },
+  {
+    "revision": "0722ebf5ceb05e52f4e2",
+    "url": "/static/js/2.0440cbc6.chunk.js"
+  },
+  {
+    "revision": "8701392bad5e4cb3a303",
+    "url": "/static/js/main.5a5a447e.chunk.js"
+  },
+  {
+    "revision": "4c38e2a7a197fad94781",
+    "url": "/static/js/runtime~main.18cf5b6d.js"
+  }
+]);
